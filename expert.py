51b3260efbd51144722e10648be80049c727012e
andreidumitras <andreei.dumitras@gmail.com>
2024-07-10 15:44:47 +0300

    facut sa mearga main

--- expert.py
+++ expert.py
@@ -90,7 +90,7 @@ class Expert:
                     },
                     {
                         "role": "assistant",
-                        "content": "percentage: 85%. The subject header impersonates Amazon, which is considered a trustworthy entity"
+                        "content": "probability: 85%. The subject header impersonates Amazon, which is considered a trustworthy entity"
                     },  
                     {
                         "role": "user",
@@ -98,13 +98,153 @@ class Expert:
                     },
                     {
                         "role": "assistant",
-                        "content": "percentage: 91%. The subject header impersonates Amazon, which is considered a trustworthy entity, and is authoritative to inform its users about their account activity"
+                        "content": "probability: 91%. The subject header impersonates Amazon, which is considered a trustworthy entity, and is authoritative to inform its users about their account activity"
+                    },  
+                    {
+                        "role": "user",
+                        "content": "Give me the percentage of how suspicious is the structure of this header"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 88%. The subject header impersonates Amazon but it contains some non-ASCII, barely visible characters between the letters of 'Amazon' word which is not normal"
                     },  
                     {
                         "role": "user",
                         "content": prompt
                     }
                 ]
+            case 'text':
+                messages = [
+                    {
+                        "role": "system",
+                        "content": 'You are specialist in phishing detection that always provide a probability that quantifies how the aspect exists in the provided input.'
+                    },
+                    {
+                        "role": "user",
+                        "content": "I will give you the following text of an email, for analysis. Focus of the phrases, because it contains some URLs:\n[![Trustwallet Logo](https://apps.uk/wp-content/uploads/2021/11/metamask-trust-wallet.jpg)](http://www.thinkific.com)  ---  |  | ###  Hi Dear; phishing@pot    ---  All unverified accounts will be suspended on 10/31/2022.  We're sorry for any inconvenience we cause with this, but please keep in mindthat our intention is to keep our customers safe and happy.  [Confirm my wallet](https://usertest.sciquest.com/apps/Router/ExternalSiteTransition?url=https://drop-coin-availablenow.site44.com/)    Thanks for doing that!  Trustwallet Team  [GetHelp](https://usertest.sciquest.com/apps/Router/ExternalSiteTransition?url=https://drop-coin-availablenow.site44.com/)   [ ContactUs](mailto:customers@Trustwallet.com)  **    qckztlefgd630ihmb87japyn254woxvuqckztlefgd630ihmb87japyn254woxvuqckztlefgd630ihmb87japyn254woxvu."
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "ok, let's see."
+                    },  
+                    {
+                        "role": "user",
+                        "content": "Give me the probability that other people are mentioned in this text"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 70%. The email ensures the user that they try to keep 'our customers happy' and speaks of others 'all unverified accounts'"
+                    },  
+                    {
+                        "role": "user",
+                        "content": "Give me the probability that this text convey a sense of urgency"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 21%. The text is kindly suggesting to the receiver to 'Confirm my wallet'"
+                    },  
+                    {
+                        "role": "user",
+                        "content": "Give me the percentage of the authoritative tone"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 15%. The tone of the email is chill and informative"
+                    },  
+                    {
+                        "role": "user",
+                        "content": "Give me the percentage of the flattery present in the email text and how much it tries to connect with the receiver"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 63%. The email starts with 'hi dear', trying to connect emotionally wirh the receiver. They thank the receiver 'thank you for doing that'"
+                    },  
+                    {
+                        "role": "user",
+                        "content": "Give me the percentage of how clear is that the receiver have to do something"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 23%. The text sais that the receiver can visit his wallet, without any other instruction"
+                    },  
+                    {
+                        "role": "user",
+                        "content": "Give me the percentage of how clear is that there is a reward or an enticing offer for the receiver"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 4%. The text is not mentioning any offer but something that the receiver can take into consideration"
+                    },  
+                    {
+                        "role": "user",
+                        "content": "Give me a percentage of how professional is the structure of the email"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 34%. The text has a more personal tone rather than a professional one"
+                    },
+                    {
+                        "role": "user",
+                        "content": "Give me a percentage of how personalized is the email"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 63%. The text has more a personal but is does not mention any personal information regarding the receiver"
+                    },
+                    {
+                        "role": "user",
+                        "content": "Give me a percentage of how vague is the tone and the content of the given email"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 70%. The text si vague because it mentions that the user's account will be suspended and not providing any reason"
+                    },
+                    {
+                        "role": "user",
+                        "content": "Give me a percentage that suggests how much the text is talking about user\'s personal information; like credit cards, passwords, accounts or a billing address"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 84%. The email mentions a 'wallet' confirmation request for the receiver"
+                    },
+                    {
+                        "role": "user",
+                        "content": "Give me the percentage of how plausible is the narrative"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 19%. The email's narrative seems to be invented, without any concrete detail and reason behind, only for the user to reveal his credentials"
+                    },
+                    {
+                        "role": "user",
+                        "content": "Give me the probability that the following text is containing grammatical errors"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 40%. The email has 2 words concatenated 'mindthat'"
+                    },
+                    {
+                        "role": "user",
+                        "content": "Give me a percentage of how suspicious are all the URLs mentioned in the email"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 100%. The email has URLs embedded inside anotehr URLs ('https://usertest.sciquest.com/apps/Router/ExternalSiteTransition?url=https://drop-coin-availablenow.site44.com/')"
+                    },
+                    {
+                        "role": "user",
+                        "content": "Give me a percentage of how suspicious for phishing is this site"
+                    },
+                    {
+                        "role": "assistant",
+                        "content": "probability: 100%. The email has URLs embedded inside anotehr URLs, vague information, grammatical problems and others cues for phishing"
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt
+                    }
+                ]
+                
         return messages
 
     def ask(self, prompt: str, about: str, quiet: bool = False) -> str:
@@ -117,6 +257,8 @@ class Expert:
         if quiet:
             return None
         answer = response.choices[0].message.content.strip().split('%')
-        self.numeric_answer = float(int(answer[0]) / 100)
-        self.explanation = answer[1].strip()
-        return list([self.numeric_answer, self.explanation])
+        index = answer[0].rfind(' ')
+        self.numeric_answer = float(int(answer[0][13:]) / 100)
+        # self.explanation = answer[1].strip()
+        # return list([self.numeric_answer, self.explanation])
+        return list([self.numeric_answer, None])
