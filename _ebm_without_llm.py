7a1ed5f500080115ef466a64c7ac42899aaab040
andreidumitras <andreei.dumitras@gmail.com>
2024-08-11 00:40:07 +0300

    ebm ready?

--- _ebm_without_llm.py
+++ _ebm_without_llm.py
@@ -1,23 +1,34 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from catboost import CatBoostClassifier
-from sklearn.model_selection import train_test_split, learning_curve
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_auc_score, RocCurveDisplay
-from skopt import BayesSearchCV
+import pandas as pd
 import sys
+import matplotlib.pyplot as plt
+from interpret.glassbox import ExplainableBoostingClassifier
+from interpret import show
+from sklearn.model_selection import train_test_split, GridSearchCV, learning_curve
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_auc_score, RocCurveDisplay
+from skopt.space import Real, Integer
+from skopt import BayesSearchCV
+from IPython.display import display
 
 def search_best_model(x_train, x_test, y_train, y_test):
 # tunning parameters:
 # Standardize the data (important for Logistic Regression)
-    model = CatBoostClassifier()
+    model = ExplainableBoostingClassifier()
     
     parameters = {
-        "iterations": [100, 200, 300, 400],
-        "depth": [4, 6, 8, 10],
-        "learning_rate": [0.01, 0.05, 0.1, 1],
-        "l2_leaf_reg": [1, 3, 5, 7, 9],
-        "border_count": [32, 64, 128]
+        'smoothing_rounds': [0, 50, 100, 200, 500, 1000, 2000, 4000],
+        'interactions': [0, 0.5, 0.75, 0.9, 0.95, 5, 10, 25, 50, 100, 250],
+        'max_bins': [1024, 4096, 16384],
+        'max_interaction_bins': [8, 16, 32, 64, 128],
+        'greedy_ratio': [0.0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 4.0],
+        'cyclic_progress': [0.0, 0.5, 1.0],
+        'interaction_smoothing_rounds': [0, 50],
+        'learning_rate': [0.1, 0.025, 0.01, 0.005, 0.0025],
+        'max_leaves': [2, 3, 4],
+        'min_samples_leaf': [2, 3, 4],
+        'early_stopping_rounds': [50],
+        'early_stopping_tolerance': [1e-5, 1e-7, 0.0],
+        'validation_size': [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4],
     }
         
     best = BayesSearchCV(
@@ -32,20 +43,36 @@ def search_best_model(x_train, x_test, y_train, y_test):
     )
     best.fit(x_train, y_train)
     
-    print("The best CatBoost model is with the following parameters:")
+    # print("The best CatBoost model is with the following parameters:")
     print(best.best_params_)
-    print(f"Score: {best.score(x_test, y_test)}")
+    print(f"Score: {model.score(x_test, y_test)}")
+    return
+    explanantions = model.explain_global()
+    print(show(explanantions))
+    y_pred = model.predict(x_test)
+    
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy: {accuracy:.4f}")
+    
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred))
+    
+    print("Confusion Matrix:")
+    print(confusion_matrix(y_test, y_pred))
+    
+    y_prob = model.predict_proba(x_test)[:, 1]
+    roc_auc = roc_auc_score(y_test, y_prob)
+    print(f"ROC-AUC: {roc_auc:.4f}")
+    
+    RocCurveDisplay.from_estimator(model, x_test, y_test)
+    plt.show()
     
 
 
 def validate_best_model(x_train, x_test, y_train, y_test):
     # Build the best model:
-    best = CatBoostClassifier(
-        border_count=64,
-        depth=8,
-        iterations=300,
-        l2_leaf_reg=7,
-        learning_rate=0.1
+    best = ExplainableBoostingClassifier(
+    
     )
     best.fit(x_train, y_train)
     
@@ -127,10 +154,6 @@ if __name__ == "__main__":
         'displayed name and local part similarity of reply',
         'from has common email provider',
         'correspondant address has common email provider',
-        'if sender impersonates something',
-        'from suspiciopus level',
-        'if reply address impersonates something',
-        'reply address suspiciopus level',
         'from full length cotinet',
         'from displayed name length cotinet',
         'from address length cotinet',
@@ -153,14 +176,6 @@ if __name__ == "__main__":
         'Subject number of lows cotient',
         'Subject is Fwd',
         'Subject is Re',
-        'Subject Authority',
-        'Subject Urgency',
-        'Subject Scarcity',
-        'Subject Social Proof',
-        'Subject Liking',
-        'Subject Reciprocity',
-        'Subject Consistancy',
-        'Subject Punctuation',
         'Text is blank',
         'Text has HTML',
         'Text number of headings cotient',
@@ -175,16 +190,6 @@ if __name__ == "__main__":
         'Text number of words cotient',
         'Text number of characters cotient',
         'Text has unusual characters',
-        'Text Authority',
-        'Text Urgency',
-        'Text Scarcity',
-        'Text Social Proof',
-        'Text Liking',
-        'Text Reciprocity',
-        'Text Consistancy',
-        'Text Punctuation',
-        'Text Grammar',
-        'Text Sensitive Information',
         'Attachments total-number cotient',
         'Inline total-number cotient',
         'Attachments variety',
